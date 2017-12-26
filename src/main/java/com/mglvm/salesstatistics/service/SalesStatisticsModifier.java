1ff114fc55a43f9f9420ec4803f692e3595b3604
Vinoth Kumar Dhanasekar <net.vinothkumar@gmail.com>
2017-12-26 12:50:27 +0100

    Removed

--- src/main/java/com/mglvm/salesstatistics/service/SalesStatisticsModifier.java
+++
@@ -1,6 +0,0 @@
-package com.mglvm.salesstatistics.service;
-
-@FunctionalInterface
-public interface SalesStatisticsModifier<SalesOrder> {
-    public void modifySalesStatistics(SalesOrder salesOrder);
-}
